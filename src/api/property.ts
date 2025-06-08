37fc1f04e6f04c8a1af138146a00367fbf4806e6
MichalWojciechowsk2 <michal.wojciechowsk2@microsoft.wsei.edu.pl>
2025-06-08 03:41:00 +0200

    Poprawione filtrowanie

--- src/api/property.ts
+++ src/api/property.ts
@@ -10,6 +10,7 @@ export const searchProperties = async (filters: any): Promise<Property[]> => {
   const response = await api.get<Property[]>("/Property/search", {
     params: filters,
   });
+  console.log(filters);
   return response.data;
 };
 
@@ -22,9 +23,10 @@ export const getCities = async (): Promise<{ id: number; name: string }[]> => {
 
 export const getDistricts = async (
   cityId: string
-): Promise<{ id: number; name: string }[]> => {
-  const response = await api.get<{ id: number; name: string }[]>(
-    `/Property/districts/${cityId}`
-  );
+): Promise<{ id: number; name: string; enumName: string }[]> => {
+  const response = await api.get<
+    { id: number; name: string; enumName: string }[]
+  >(`/Property/districts/${cityId}`);
+  console.log(response.data);
   return response.data;
 };
