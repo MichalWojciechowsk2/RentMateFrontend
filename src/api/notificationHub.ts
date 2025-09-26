1115f13e31ef35279a22638608bb462070c47007
MichalWojciechowsk2 <michal.wojciechowsk2@microsoft.wsei.edu.pl>
2025-09-26 23:33:46 +0200

    Fixing signal R attempt 2

--- src/api/notificationHub.ts
+++ src/api/notificationHub.ts
@@ -3,25 +3,28 @@ import type { NotificationEntity } from "../types/Notifications";
 
 let connection: signalR.HubConnection | null = null;
 
-export const startNotificationHub = (token?: string) => {
+export const startNotificationHub = async (token?: string) => {
   if (!connection) {
     connection = new signalR.HubConnectionBuilder()
-      .withUrl("https://localhost:5001/hubs/notifications", {
+      .withUrl("https://localhost:7281/hubs/notifications", {
         accessTokenFactory: () => token || "",
       })
       .withAutomaticReconnect()
       .build();
 
-    connection
-      .start()
-      .then(() => console.log("Połączono z signalR"))
-      .catch((err) => console.error("Błąd połączenia z signalR: ", err));
+    await connection.start();
   }
-  console.log("Mam połączenie", connection);
   return connection;
 };
 
 export const onReceiveUnreadCount = (callback: (count: number) => void) => {
   if (!connection) return;
   connection.on("ReceiveUnreadCount", callback);
+};
+
+export const stopNotificationHub = async () => {
+  if (connection) {
+    await connection.stop();
+    connection = null;
+  }
 };
