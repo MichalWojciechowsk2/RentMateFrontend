3040296ffb3745d538748203093c1b0fa127ea85
MichalWojciechowsk2 <michal.wojciechowsk2@microsoft.wsei.edu.pl>
2025-10-12 22:36:07 +0200

    Send message view

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -80,7 +80,7 @@ const NavBar: React.FC = () => {
           </Link>
         )}
         {user && (
-          <Link to="/messages" className="relative flex items-center">
+          <Link to="/chats" className="relative flex items-center">
             <FaMessage className="text-xl" />
             {unreadMessages > 0 && (
               <span className="absolute -top-1 -right-1 bg-red-600 text-white rounded-full text-xs w-5 h-5 flex items-center justify-center">
